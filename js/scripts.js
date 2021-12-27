d9fd837f9c3d21ed2f9c51c2b28d2d0a697bcfae
Luis Grimaldo <lg@gold.local>
2021-12-26 21:01:40 -0800

    wip

--- js/scripts.js
+++ js/scripts.js
@@ -91,28 +91,10 @@ $(document).ready(function () {
 
             if (scroll >= 20) {
                 $('section.navigation').addClass('fixed');
-                $('header').css({
-                    "border-bottom": "none",
-                    "padding": "35px 0"
-                });
-                $('header .member-actions').css({
-                    "top": "26px",
-                });
-                $('header .navicon').css({
-                    "top": "34px",
-                });
+                $('.title').css({fontSize:'3vw'});
             } else {
                 $('section.navigation').removeClass('fixed');
-                $('header').css({
-                    "border-bottom": "solid 1px rgba(255, 255, 255, 0.2)",
-                    "padding": "50px 0"
-                });
-                $('header .member-actions').css({
-                    "top": "41px",
-                });
-                $('header .navicon').css({
-                    "top": "48px",
-                });
+                $('.title').css({fontSize:'5vw'});
             }
         });
     });
@@ -128,7 +110,7 @@ $(document).ready(function () {
                 if (target.length) {
                     $('html,body').animate({
                         scrollTop: target.offset().top - 90
-                    }, 2000);
+                    }, 1000);
                     return false;
                 }
             }
